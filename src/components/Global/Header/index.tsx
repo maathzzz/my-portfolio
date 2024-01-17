0666d86678f43ecbfee254c8ed2a4b4e081b11f9
Matheus A. Favero <matheusdeamorim20@gmail.com>
2024-01-17 12:07:53 -0300

    feat: hide-experiment

--- src/components/Global/Header/index.tsx
+++ src/components/Global/Header/index.tsx
@@ -7,9 +7,9 @@ export default function Header() {
                 <div className="text-3xl font-bold text-[#cc063e] invisible"><span className="text-[#fd9407]">M</span></div>
 
                 <div className="flex flex-row items-center gap-4">
-                    <a className="text-xs font-semibold text-[#141414] duration-75 dark:text-white cursor-pointer dark:hover:text-[#fd9407]">SOBRE</a>
+                    {/* <a className="text-xs font-semibold text-[#141414] duration-75 dark:text-white cursor-pointer dark:hover:text-[#fd9407]">SOBRE</a>
                     <a className="text-xs font-semibold text-[#141414] duration-75 dark:text-white cursor-pointer dark:hover:text-[#fd9407]">PROJETOS</a>
-                    <a className="text-xs font-semibold text-[#141414] duration-75 dark:text-white cursor-pointer dark:hover:text-[#fd9407]">CONTATO</a>
+                    <a className="text-xs font-semibold text-[#141414] duration-75 dark:text-white cursor-pointer dark:hover:text-[#fd9407]">CONTATO</a> */}
                     <SwitchButton />
                 </div>
             </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,31 +1,42 @@
 import DefaultLayout from '@/layouts/DefaultLayout'
-import React, { useState } from 'react'
+import React, { use, useEffect, useState } from 'react'
 import { motion, AnimatePresence } from 'framer-motion'
-import styles from '@/styles/home.module.css'
+import styles from '@/styles/home.module.scss'
+import useMousePosition from '@/utils/useMousePosition';
 
 export default function Home() {
-    return (
-        <DefaultLayout>
-            <section className='w-full min-h-[40rem] border-b-2 flex justify-center items-center'>
-                <div className='flex flex-col justify-center'>
-                    <span className='items-start text-xl font-bold pl-1 text-[#005b96]'>hey, i'm a</span>
-                    <span className={`text-8xl font-black ${styles.toRightGradient}`}>FULLSTACK <strong className='text-[#cc063e]'>&</strong></span>
-                    <span className='text-8xl font-black'><strong className='text-[#fd9407]'>GAME</strong> <strong className={`${styles.toLeftGradient}`}>DEV</strong></span>
-                </div>
-            </section>
-            <section className='p-24'>
-                <motion.div
-                    className='w-[10rem] h-[10rem] bg-black dark:bg-white rounded-3xl '
-                    whileHover={{ scale: 1.2, rotate: 90 }}
-                    whileTap={{
-                        scale: 0.8,
-                        rotate: -90,
-                        borderRadius: "100%"
-                    }}
-                >
-                </motion.div>
+    // var cursor = document.querySelector(".cursor");
+    // var cursor2 = document.querySelector(".cursor2");
+    // document.addEventListener("mousemove", function (e) {
+    //     cursor.style.cssText = cursor2.style.cssText = "left: " + e.clientX + "px; top: " + e.clientY + "px;";
+    // });
 
-            </section>
-        </DefaultLayout>
+    const { x, y } = useMousePosition();
+    const size = 400
+
+    useEffect(() => {
+        console.log(`Posição do mouse: ${x}, ${y}`)
+    }, [x, y]);
+
+    return (
+        <motion.div
+            className={`${styles.mask} bg-black dark:bg-white`}
+            animate={{
+                WebkitMaskPosition: `${x - size / 2}px ${y - size / 2}px`,
+            }}
+            transition={{ type: "tween", ease: "backOut" }}
+        >
+            <DefaultLayout>
+                <section className='w-full min-h-[40rem] border-b-2 flex justify-center items-center'>
+                    <div className='flex flex-col justify-center'>
+                        <span className='items-start text-xl font-bold pl-1 text-[#005b96]'>hey, i'm a</span>
+                        <span className={`text-7xl font-black ${styles.toRightGradient}`}>FULLSTACK <strong className='text-[#fd9407]'>WEB</strong> </span>
+                        <span className='text-7xl font-black'><strong className='text-[#cc063e]'>&</strong> <strong className='text-[#fd9407]'>GAME</strong> <strong className={`${styles.toLeftGradient}`}>DEVELOPER</strong></span>
+                    </div>
+                    <div className="cursor"></div>
+                    <div className="cursor2"></div>
+                </section>
+            </DefaultLayout>
+        </motion.div>
     )
 }
